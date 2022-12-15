96d56b710f3d2cae9ade96faed1e30163b3d1728
HNNNY35 <hnnny35@gmail.com>
2022-12-15 09:30:36 +0900

    first commit

---
+++ src/main/java/com/hnnny/toy_servlets/ToyServletsApplication.java
@@ -0,0 +1,13 @@
+package com.hnnny.toy_servlets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToyServletsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToyServletsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hnnny/toy_servlets/ToyServletsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hnnny.toy_servlets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToyServletsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
